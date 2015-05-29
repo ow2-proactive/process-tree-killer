1c2924f7997af164e748a1f6fbb0123fa618d9f8
Laurent Pellegrino <laurent.pellegrino@activeeon.com>
2015-05-29 14:35:49 +0200

    Add tests for Windows

--- src/test/java/org/ow2/proactive/process_tree_killer/ProcessTreeTest.java
+++ src/test/java/org/ow2/proactive/process_tree_killer/ProcessTreeTest.java
@@ -1,12 +1,14 @@
 package org.ow2.proactive.process_tree_killer;
 
+import org.junit.Test;
+
 import java.io.File;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
-
-import org.junit.Test;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static org.junit.Assert.*;
@@ -42,8 +44,8 @@ public class ProcessTreeTest {
 
     @Test
     public void killSingleProcess_UsingEnvironmentVariable() throws Exception {
-        Map<String, String> processTreeKillerCookie = Collections.singletonMap("PROCESS_TREE_KILLER_COOKIE",
-                "42");
+        Map<String, String> processTreeKillerCookie =
+                Collections.singletonMap("PROCESS_TREE_KILLER_COOKIE", "42");
 
         ProcessBuilder processBuilder = createSleepyProcess();
         processBuilder.environment().putAll(processTreeKillerCookie);
@@ -71,8 +73,7 @@ public class ProcessTreeTest {
     public void killProcessTree() throws Exception {
         assumeTrue(!isWindowsOS());
 
-        Process process = new ProcessBuilder("bash", new File(getClass().getResource(
-                "/create_process_tree.sh").toURI()).getAbsolutePath()).start();
+        Process process = createScriptProcess("create_process_tree").start();
 
         List<ProcessTree.OSProcess> children = waitChildProcessRunning(process, 2);
 
@@ -86,11 +87,51 @@ public class ProcessTreeTest {
     }
 
     @Test
+    public void killProcessTree_EnvironmentVariables_Windows() throws Exception {
+        Map<String, String> processTreeKillerCookie =
+                Collections.singletonMap("PROCESS_TREE_KILLER_COOKIE", "42");
+
+        ProcessBuilder createProcessTree = createScriptProcess("create_process_tree");
+        createProcessTree.environment().putAll(processTreeKillerCookie);
+        Process process = createProcessTree.start();
+
+        // Process tree expected:
+        //  cmd.exe
+        //  | conhost.exe
+        //  | +-- start /B cmd.exe /C
+        //        | +-- ping.exe
+        //  | ping.exe
+        List<ProcessTree.OSProcess> children = waitChildProcessRunning(processTreeKillerCookie, 5);
+
+        ProcessTree.get().killAll(processTreeKillerCookie);
+
+        int exitCode = process.waitFor();
+        assertNotEquals(0, exitCode);
+
+        for (ProcessTree.OSProcess child : children) {
+            assertFalse(isProcessRunning(child));
+        }
+    }
+
+    private ProcessBuilder createScriptProcess(String scriptName) throws URISyntaxException {
+        if (isWindowsOS()) {
+            return createScriptProcess(scriptName + ".bat", "cmd.exe", "/C");
+        } else {
+            return createScriptProcess(scriptName + ".sh", "bash");
+        }
+    }
+
+    private ProcessBuilder createScriptProcess(String scriptName, String... shell) throws URISyntaxException {
+        ProcessBuilder processBuilder = new ProcessBuilder(shell);
+        processBuilder.command().add(new File(getClass().getResource("/" + scriptName).toURI()).getAbsolutePath());
+        return processBuilder;
+    }
+
+    @Test
     public void killProcessTree_Detached() throws Exception {
         assumeTrue(!isWindowsOS());
 
-        Process process = new ProcessBuilder("bash", new File(getClass().getResource(
-                "/create_detached_process_tree.sh").toURI()).getAbsolutePath()).start();
+        Process process = createScriptProcess("create_detached_process_tree").start();
 
         List<ProcessTree.OSProcess> children = waitChildProcessRunning(process, 2);
 
@@ -108,7 +149,7 @@ public class ProcessTreeTest {
     }
 
     private List<ProcessTree.OSProcess> waitChildProcessRunning(final Process process,
-            final int expectedNumberOfChildProcesses) {
+                                                                final int expectedNumberOfChildProcesses) {
 
         await().until(new Callable<Boolean>() {
             @Override
@@ -119,6 +160,32 @@ public class ProcessTreeTest {
         });
         return ProcessTree.get().get(process).getChildren();
     }
+
+    private List<ProcessTree.OSProcess> waitChildProcessRunning(final Map<String, String> environmentVariables,
+                                                                final int expectedNumberOfChildProcesses) {
+
+        await().until(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                int nbProcessFound = 0;
+                for (ProcessTree.OSProcess osProcess : ProcessTree.get().processes.values()) {
+                    if (osProcess.hasMatchingEnvVars(environmentVariables)) {
+                        nbProcessFound++;
+                    }
+                }
+                return nbProcessFound == expectedNumberOfChildProcesses;
+            }
+        });
+
+        List<ProcessTree.OSProcess> childProcesses = new ArrayList<>();
+        for (ProcessTree.OSProcess osProcess : ProcessTree.get().processes.values()) {
+            if (osProcess.hasMatchingEnvVars(environmentVariables)) {
+                childProcesses.add(osProcess);
+            }
+        }
+        return childProcesses;
+    }
+
 
     private ProcessBuilder createSleepyProcess() {
         if (isWindowsOS()) {
@@ -131,4 +198,5 @@ public class ProcessTreeTest {
     private boolean isWindowsOS() {
         return System.getProperty("os.name").toLowerCase().contains("windows");
     }
+
 }
---
+++ src/test/resources/create_process_tree.bat
@@ -0,0 +1,4 @@
+start /B cmd.exe /C ping 127.0.0.1 -n 10000 > NUL 2>&1
+
+ping 127.0.0.1 -n 10000 > NUL 2>&1
+
